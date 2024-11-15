b2fcdf23b6cb1843528798b7db25e108a332b083
Navid Ibrahim <nibrahim7787@gmail.com>
2024-11-15 20:05:52 +0600

    Created abstract User class

---
+++ main/java/User.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+abstract class User implements Readable {
+    private String userId;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String userId, String username, String email, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    @Override
+    public void readData(String filePath) throws IOException {
+        FileUtility.readFile(filePath);
+    }
+}
